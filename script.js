bfa3836a3b2488287a0d2771d46e22e1d250b035
Christopher Werkmeister <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-11-01 10:08:50 +1000

    style inputs

--- script.js
+++ script.js
@@ -12,8 +12,12 @@ confirmPasswordInput.addEventListener("input", () => {
   if (password !== confirmPassword) {
     passwordError.classList.add("error"); // Add error class if passwords don't match
     passwordError.innerText = "* Passwords do not match"; // Display error message
+    confirmPasswordInput.style.outline = "none";
+    confirmPasswordInput.style.boxShadow = "none";
+    confirmPasswordInput.style.border = "2px solid red";
   } else {
     passwordError.classList.remove("error"); // Remove error class if passwords match
+    confirmPasswordInput.style.border = "1px solid #000000";
     passwordError.innerText = ""; // Clear error message if passwords match
   }
 });
